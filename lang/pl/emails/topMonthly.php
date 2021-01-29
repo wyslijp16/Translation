fb6510c1c92a482f3d86a4cd23654ddf42aad680
wyslijp16 <wyslijp16@gmail.com>
2021-01-29 17:39:48 +0100

    do dokończenia

--- lang/pl/emails/topMonthly.php
+++ lang/pl/emails/topMonthly.php
@@ -1,17 +1,17 @@
 <?php
 
-    $TITLE = "CLASSEMENT DU MOIS 🏆";
+    $TITLE = "RANKING MIESIĄCA 🏆";
     $P1 = "Wow.. ! On vient de me dire que nous étions le 1 er du mois, ce qui veux dire : la petite routine du début de mois !! 🤭";
     $P2 = "Comme tous les mois, nous remettons les votes à zéro, cela permet aux nouveaux d'avoir plus de faciliter à s'intégrer dans la course au classement et de challenger vos membres tous les mois !";
-    $BUTTON_JOIN = "REJOINDRE CE SERVEUR";
-    $BUTTON_SEE = "VOIR";
-    $BUTTON_SEE_ALL = "VOIR LE CLASSEMENT";
+    $BUTTON_JOIN = "DOŁĄCZ DO TEGO SERWERA";
+    $BUTTON_SEE = "ZOBACZ";
+    $BUTTON_SEE_ALL = "ZOBACZ RANKING";
 
-    $KNOW_TITLE = "Le saviez-vous ? 🤷‍♂️";
-    $KNOW_DESC = "Nous avons un serveur discord dans lequel nous vous partageons encore plus d'information que par email ! Il vaut le détour !";
-    $KNOW_BUTTON = "Rejoindre notre Discord !";
+    $KNOW_TITLE = "Czy wiedziałeś/aś ? 🤷‍♂️";
+    $KNOW_DESC = "Posiadamy serwer discord na którym udostępniamy wam jeszcze więcej informacji niż na emailu ! Wymaga uwagi !";
+    $KNOW_BUTTON = "Dołącz na naszego Discorda !";
 
-    $FOOTER_TITLE = "Besoin d'aide ?";
-    $FOOTER_DESC = "Nous sommes là, prêt à t'aider !";
+    $FOOTER_TITLE = "Potrzebna pomoc ?";
+    $FOOTER_DESC = "Jesteśmy tu, by ci pomóc !";
 
 ?>
