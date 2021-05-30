11bc317fdc8d6f85830d343e619c47cca71d8fa4
wyslijp16 <wyslijp16@gmail.com>
2021-05-30 20:07:03 +0200

    do dokończenia

--- lang/pl/emails/addCredit.php
+++ lang/pl/emails/addCredit.php
@@ -1,18 +1,18 @@
 <?php
 
-    $TITLE = "Votre compte a été crédité ! 🤑";
-    $P1 = "Bonjour, %username% !";
-    $P2 = "Nous vous confirmons avec ce mail la reception de vos %CASH% CASH sur votre compte DiscordTop.";
-    $P3 = "Nous tenions également à vous remerciez, si nous existons, c'est en parti grace à des personnes comme vous !";
+    $TITLE = "Twoje konto zostało zkredytowane ! 🤑";
+    $P1 = "Witaj, %username% !";
+    $P2 = "Potwierdzamy ci tym mailem uzyskanie twych %CASH% CASH na twoim koncie DiscordTop.";
+    $P3 = "Chcielibyśmy też ci podziękować, jeżeli istniejemy, to też dzięki osobom takim jak ty !";
     $P4 = "Vous pouvez réclamer un grade \"Client\" sur notre Discord, il suffit d'ouvrir un ticket et d'en faire la demande, c'est très simple et rapide pour pouvez arborer fièrement en grade client et vous la péter auprès de vos amis (encore faut-il en avoir) !";
-    $P5 = "Merci de votre soutiens,<br>Cordialement,<br>L'Équipe de DiscordTop.Org";
+    $P5 = "Dziękujemy za twoje wsparcie,<br>Serdecznie,<br>Zespół DiscordTop.Org";
 
-    $KNOW_TITLE = "Le saviez-vous ? 🤷‍♂️";
+    $KNOW_TITLE = "Czy wiedziałeś/aś ? 🤷‍♂️";
     $KNOW_DESC = "Sur notre serveur Discord, il est régulier d'avoir des bons plans pour payer moins chère ! Entre les AirDrops et les cadeaux, qu'est-ce que vous attendez pour nous rejoindre ?";
-    $KNOW_BUTTON = "Rejoindre notre Discord !";
+    $KNOW_BUTTON = "Dołącz do nas na Discord !";
 
-    $FOOTER_TITLE = "Besoin d'aide ?";
-    $FOOTER_DESC = "Nous sommes là, prêt à t'aider !";
+    $FOOTER_TITLE = "Potrzebna pomoc ?";
+    $FOOTER_DESC = "Jesteśmy tu, gotowi by ci pomóc !";
 
 
 ?>
